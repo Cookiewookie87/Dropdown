ef30cc340536b120f3af2447f150e72dbeea1f58
Cookiewookie87 <jernej.tratnik@gmail.com>
2018-04-19 15:17:58 +0200

    Added functionality for dynamic opening of dropdowns

--- js/script.js
+++ js/script.js
@@ -3,14 +3,31 @@ let dropdownInput = document.querySelectorAll(".dropdown-input");
 let dropdownItem = document.querySelectorAll(".dropdown-item");
 
 for (let j = 0; j < dropdownInput.length; j++) {
+    
+    let currentDropdownSelection = dropdownSelection[j];
     dropdownInput[j].addEventListener("click", function(){
         dropdownSelection[j].classList.toggle("active");
+        
+        dropdownSelection.forEach(function(item) {
+            if (event.target.nextSibling.nextElementSibling !== item &&
+               item.classList.contains("active"))
+                {
+                    item.classList.remove("active")
+                }
+            console.log(item.classList.contains("active"));
+            console.log(event.target.nextElementSibling);
+        });
     });
 }
 
 for (let i = 0; i < dropdownItem.length; i++){
     dropdownItem[i].addEventListener("click", function (){
-        dropdownItem[i].parentNode.parentNode.childNodes[1].textContent = dropdownItem[i].textContent;
-        dropdownSelection[i].classList.toggle("active"); 
+        //get li items content, get li item previous sibling
+        event.target.parentElement.previousSibling.previousSibling.textContent = event.target.textContent;
+        event.target.parentElement.classList.remove("active");
+        
+        //For testing
+        //console.log(event.target.textContent);
+        //console.log(event.target.parentElement.previousSibling.previousSibling);
     });
 }
